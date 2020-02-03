823c00a81a072743ee44c0f7f1ac379bdc0fea21
Elmer C. Galo <elmer.galo@outlook.com>
2020-02-03 01:08:55 -0500

    Create route for register/save a new user to DB

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -13,7 +13,7 @@ function NavBar() {
                 <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
                 <a className="nav-item nav-link" href="#">Features</a>
                 <a className="nav-item nav-link" href="#">Pricing</a>
-                <a className="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a className="nav-item nav-link disabled" href="#" aria-disabled="true">Disabled</a>
             </div>
         </div>
     </nav>
--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
+import API from "../utils/API";
 
-function landingPage() {
-	return (
-		<div>
-			<h1>Landing Page</h1>
-		</div>
-	);
+class index extends Component {
+	state = {
+		email: "",
+		password: ""
+	};
+
+	handleInputChange = event => {
+		const { name, value } = event.target;
+		this.setState({
+			[name]: value
+		});
+	};
+
+	handleFormSubmit = event => {
+		event.preventDefault();
+		if (this.state.email && this.state.password) {
+			API.saveUser({
+				email: this.state.email,
+				password: this.state.password
+			})
+				.then(res => console.log(res.data))
+				.catch(err => console.log(err));
+		}
+	};
+
+	render() {
+		return (
+			<div className="container rounded shadow p-4 mt-4 w-25">
+				<h4>Register</h4>
+				<form>
+					<div className="form-group">
+						<label htmlFor="email">Email address</label>
+						<input id="email" type="email" className="form-control" value={this.state.email} onChange={this.handleInputChange} name="email" />
+					</div>
+					<div className="form-group">
+						<label htmlFor="password">Password</label>
+						<input id="password" type="password" className="form-control" value={this.state.password} onChange={this.handleInputChange} name="password" />
+					</div>
+					<button type="submit" className="btn btn-primary" onClick={this.handleFormSubmit}>
+						Submit
+					</button>
+				</form>
+			</div>
+		);
+	}
 }
 
-export default landingPage;
+export default index;
