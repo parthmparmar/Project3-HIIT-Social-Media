781de009cc7e7186f1184ceeb28fc6faeb281f04
cvevera <cvevera@gmail.com>
2020-02-04 12:49:22 -0500

    updated user model

--- project3/src/models/user.js
+++ project3/src/models/user.js
@@ -5,9 +5,8 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: {type: String, requlred: true },
-  age: { type: String, required: true },
-  gender: { type : String, required: true },
-  
+  age: { type: String, required: false },
+  gender: { type : String, required: false }
 });
 
 const User = mongoose.model("User", UserSchema);
