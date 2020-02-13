1129abfd5f143c9aa76b60c12c6e8ee7906f349b
Parth Parmar <parthmparmar@gmail.com>
2020-02-12 19:19:29 -0500

    added method to locate selected user and display name on page

--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -3,14 +3,14 @@ import API from "../utils/API";
 import { Redirect } from "react-router-dom";
 import "./styles/landing.css";
 import Row from "../components/Row"
-import { set } from "mongoose";
 import MembersCard from "../components/MembersCard";
-import Avatar from 'avataaars'
+import Avatar from 'avataaars';
 
 class Members extends Component {
 	state = {
         dbMembers : [],
-        clickedId : ""
+        clickedId : "",
+        display: false
     }
 
     componentDidMount(){
@@ -24,6 +24,20 @@ class Members extends Component {
         });
     }
 
+    setClickedId = (index) => {
+        let newState = {
+            clickedId: index,
+            display: true
+        }
+        this.setState(newState);
+    }
+
+    showName = () => {
+        if (parseInt(this.state.clickedId) > -1){
+            return(<h3>{this.state.dbMembers[this.state.clickedId].firstName}</h3>)
+        }
+    }
+
     render() {
 		if (this.state.redirect) {
 			return <Redirect to={this.state.redirect} />;
@@ -33,11 +47,12 @@ class Members extends Component {
             <div className="members-page">
 
                 <Row>
-                    {this.state.dbMembers.map((userData) => {
-                        return <MembersCard userData={userData}/>
+                    {this.state.dbMembers.map((userData, index) => {
+                        return <MembersCard userData={userData} key={index} id={index} click={this.setClickedId}/>
                     })}
                 </Row>
-
+                
+                    {this.showName()}
             </div>
 
         )
